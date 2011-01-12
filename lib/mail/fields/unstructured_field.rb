314e05412179739091ddbff6b7c3362c93561472
Conor Hunt <conor.hunt+git@gmail.com>
2011-01-11 23:00:13 -0500

    fix issue with unstructured field decoding

--- lib/mail/fields/unstructured_field.rb
+++ lib/mail/fields/unstructured_field.rb
@@ -72,7 +72,7 @@ module Mail
     
     def do_decode
       result = value.blank? ? nil : Encodings.decode_encode(value, :decode)
-      result.encode!(value.encoding || "UTF-8") if RUBY_VERSION >= '1.9' && !result.blank?
+      #result.encode!(value.encoding || "UTF-8") if RUBY_VERSION >= '1.9' && !result.blank?
       result
     end
     
